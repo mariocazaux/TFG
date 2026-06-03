a537b796aafafeaf988f0ef852a67183ce965339
MarioCazaux <mcnc1@gcloud.ua.es>
2026-06-03 19:57:09 +0200

    pwa doninio customs cors

--- dev/backend/src/index.ts
+++ dev/backend/src/index.ts
@@ -1,7 +1,23 @@
 import express, { type Request, type Response } from 'express';
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Configuración de CORS
+const allowedOrigins = ['http://localhost:4200', 'https://kachaoo.com'];
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      // Permite peticiones sin origin (ej. Postman) o si están en la lista permitida
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('No permitido por CORS'));
+      }
+    },
+  }),
+);
 
 // Middleware para entender cuerpos JSON en las peticiones
 app.use(express.json());
